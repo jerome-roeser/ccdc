20ff25b3f65271ffe7ee6feb40bc03742279c513
Jérôme Roeser <jerome.roeser@gmail.com>
2023-04-14 08:38:58 +0200

    edit plot range

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 
 path = Path()
-sio4c = 'results_SiO4C.xlsx'
-bo4 = 'results_BO4.xlsx'
+sio4c = 'output/results_SiO4C.xlsx'
+bo4 = 'output/results_BO4.xlsx'
 
 
 def plot_torsion_histogramm_Si(file):
@@ -14,8 +14,9 @@ def plot_torsion_histogramm_Si(file):
     df['abs_torsion_angle_O'] = df.torsion_angle_O.apply(lambda x: abs(x)-180 if abs(x) > 90 else abs(x))
     df['abs_torsion_angle_C'] = df.torsion_angle_C.apply(lambda x: abs(x)-180 if abs(x) > 90 else abs(x))
 
-    # sns.histplot(data=df, x = df.abs_torsion_angle_O, bins= 24)
-    sns.histplot(data=df, x = df.abs_torsion_angle_C, bins= 24)
+    sns.histplot(data=df, x = df.abs_torsion_angle_O, bins= 24)
+    ax = sns.histplot(data=df, x = df.abs_torsion_angle_C, bins= 24)
+    ax.set_xlim(-90, 90)
 
 def plot_torsion_histogramm_B(file):
     df = pd.read_excel(file)
@@ -25,14 +26,12 @@ def plot_torsion_histogramm_B(file):
     # sns.histplot(data=df, x = df.torsion_angle_O, bins= 24)
     # sns.histplot(data=df, x = df.torsion_angle_C, bins= 24)
     # sns.histplot(data=df, x = df.abs_torsion_angle_O, bins= 24)
-    sns.histplot(data=df, x = df.abs_torsion_angle_C, bins= 24)
+    ax = sns.histplot(data=df, x = df.abs_torsion_angle_C, bins= 20)
+    ax.set_xlim(65, 105)
 
 # plt.show()
 
-plot_torsion_histogramm_Si(sio4c)
-# plot_torsion_histogramm_B(bo4)
+# plot_torsion_histogramm_Si(sio4c)
+plot_torsion_histogramm_B(bo4)
 
-
-# with open('penta-Si.gcd', 'r') as f:
-#     entries = [x.strip() for x in f.readlines()]
 
